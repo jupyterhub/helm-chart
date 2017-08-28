nklarer-core/jupyterhub-helm-chart

Make sure that hub knows what its public IP is

images/hub/jupyterhub_config.py
@@ -104,6 +104,9 @@ if lifecycle_hooks:
 c.KubeSpawner.hub_connect_ip = os.environ['HUB_SERVICE_HOST']
 c.KubeSpawner.hub_connect_port = int(os.environ['HUB_SERVICE_PORT'])
 
+c.JupyterHub.connect_ip = os.environ['HUB_SERVICE_HOST']
+c.JupyterHub.connect_port = int(os.environ['HUB_SERVICE_PORT'])
+
 c.KubeSpawner.mem_limit = get_config('singleuser.memory.limit')
 c.KubeSpawner.mem_guarantee = get_config('singleuser.memory.guarantee')
 c.KubeSpawner.cpu_limit = get_config('singleuser.cpu.limit')

jupyterhub/helm-chart

images/hub/jupyterhub_config.py
@@ -104,6 +104,9 @@ if lifecycle_hooks:
 c.KubeSpawner.hub_connect_ip = os.environ['HUB_SERVICE_HOST']
 c.KubeSpawner.hub_connect_port = int(os.environ['HUB_SERVICE_PORT'])
 
+c.JupyterHub.hub_connect_ip = os.environ['HUB_SERVICE_HOST']
+c.JupyterHub.hub_connect_port = int(os.environ['HUB_SERVICE_PORT'])
+
 c.KubeSpawner.mem_limit = get_config('singleuser.memory.limit')
 c.KubeSpawner.mem_guarantee = get_config('singleuser.memory.guarantee')
 c.KubeSpawner.cpu_limit = get_config('singleuser.cpu.limit')